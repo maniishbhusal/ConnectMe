17d007f456bd39ccd8656c931b041de073033587
Manish Bhusal <manishbhusal6@gmail.com>
2024-10-24 17:56:21 +0545

    feat: implement user registration API

---
+++ backend/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,12 @@
 
+using backend.Context;
+using backend.Models.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace backend
 {
     public class Program
@@ -14,6 +22,23 @@ namespace backend
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddIdentityApiEndpoints<AppUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            // For testing only Testing --  Configuring Identity options (password, email uniqueness, etc.)
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.User.RequireUniqueEmail = true;
+            });
+
+            // Configuring Entity Framework to use SQL Server
+            builder.Services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLAuthConnection"))
+            );
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -27,10 +52,50 @@ namespace backend
 
             app.UseAuthorization();
 
+            app.MapGroup("/api")
+                .MapIdentityApi<AppUser>();
+
+            // API route for user registration
+            app.MapPost("/api/signup", async (
+                UserManager<AppUser> userManager,
+                [FromBody] UserRegistrationModel userRegistrationModel
+                ) =>
+            {
+                AppUser user = new AppUser()
+                {
+                    UserName = userRegistrationModel.Username,
+                    Email = userRegistrationModel.Email,
+                    FullName = userRegistrationModel.FullName,
+                    Gender = userRegistrationModel.Gender,
+                    DateOfBirth = userRegistrationModel.DateOfBirth,
+                };
+                // Password confirmation check
+                if (userRegistrationModel.Password != userRegistrationModel.Password2)
+                {
+                    return Results.BadRequest(new { message = "Password do not match." });
+                }
+                var result = await userManager.CreateAsync(user, userRegistrationModel.Password);
+                if (result.Succeeded)
+                    return Results.Ok(result);
+                else
+                    return Results.BadRequest(result);
+            });
 
             app.MapControllers();
 
             app.Run();
+        }
+
+        // Model for User Registration
+        public class UserRegistrationModel
+        {
+            public string Email { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string Password2 { get; set; }
+            public string FullName { get; set; }
+            public string Gender { get; set; }
+            public DateOnly DateOfBirth { get; set; }
         }
     }
 }
