9c11d9bc867949e46abf6f35280df983ae29ab16
Manish Bhusal <manishbhusal6@gmail.com>
2024-10-25 18:12:31 +0545

    setup tailwind and shadcn with react

---
+++ frontend/src/components/layout/RootLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "../../pages/HomePage"
+
+const RootLayout = () => {
+  return (
+    <>
+      <HomePage />;
+    </>
+  );
+};
+
+export default RootLayout;
