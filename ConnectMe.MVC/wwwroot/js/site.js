608013ac92a15aa14ab42bd112ebf39598037297
Manish Bhusal <manishbhusal6@gmail.com>
2025-01-23 10:00:14 +0545

    customize navbar and added post creation form open on button click

--- ConnectMe.MVC/wwwroot/js/site.js
+++ ConnectMe.MVC/wwwroot/js/site.js
@@ -1,4 +1,67 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function editPost(postId) {
+    $.get('/Posts/Edit/' + postId)
+        .done(function (response) {
+            if (response.success) {
+                $('#editPostId').val(postId);
+                $('#editTitle').val(response.post.title);
+                $('#editDescription').val(response.post.description);
+                $('#editModal').modal('show');
+            }
+        });
+}
 
-// Write your JavaScript code.
+function updatePost() {
+    var postId = $('#editPostId').val();
+    var formData = new FormData($('#editForm')[0]);
+
+    $.ajax({
+        url: '/Posts/Edit/' + postId,
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        headers: {
+            'RequestVerificationToken': $('input[name="__RequestVerificationToken"]').val()
+        },
+        success: function (response) {
+            if (response.success) {
+                location.reload();
+            } else {
+                alert(response.message || 'Error updating post');
+            }
+        },
+        error: function () {
+            alert('Error updating post');
+        }
+    });
+}
+
+function deletePost(postId) {
+    if (confirm('Are you sure you want to delete this post?')) {
+        $.post('/Posts/Delete/' + postId, {
+            '__RequestVerificationToken': $('input[name="__RequestVerificationToken"]').val()
+        })
+            .done(function (response) {
+                if (response.success) {
+                    location.reload();
+                } else {
+                    alert('Error deleting post');
+                }
+            })
+            .fail(function () {
+                alert('Error deleting post');
+            });
+    }
+}
+
+// Functions for post creation appear when + button is clicked
+function togglePostForm() {
+    const form = document.getElementById('postCreationForm');
+    form.classList.toggle('d-none'); // Toggle visibility
+}
+
+// Close button functionality
+document.getElementById('closePostForm').addEventListener('click', () => {
+    const form = document.getElementById('postCreationForm');
+    form.classList.add('d-none'); // Hide form
+});
